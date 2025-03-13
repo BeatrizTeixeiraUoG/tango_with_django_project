3cff408cb1e25fc1459bf40548d57472d0fe84bf
“Beatriz <3035152t@student.gla.ac.uk>
2025-03-13 11:15:51 +0000

    chapter 10 exercises + successful tests

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 
 LOGIN_URL = 'rango:login'
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
